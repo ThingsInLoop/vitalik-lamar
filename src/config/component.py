5925f262d3665a27a74da3650fdd8e2ce64ce323
Sasha Kamalov <ishot83@gmail.com>
2024-12-29 16:10:53 +0300

    static config

--- src/config/component.py
+++ src/config/component.py
@@ -10,19 +10,10 @@ class ConfigComponent:
     def __init__(self, config_path):
         self.config_path = config_path
         self.config = self.read_config()
-        self.periodic_update = asyncio.create_task(
-            self.update_config(),
-            name="update-config",
-        )
 
     def read_config(self):
         with open(self.config_path, mode='r', encoding='utf-8') as config_yaml:
             return yaml.safe_load(config_yaml)
-
-    async def update_config(self, period_sec=5):
-        while True:
-            await asyncio.sleep(period_sec)
-            self.config = self.read_config()
 
     def get_config(self):
         return self.config
