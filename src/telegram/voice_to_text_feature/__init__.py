cccde4deca13e5a400fd7c7180a76d48c86d413d
Sasha Kamalov <ishot83@gmail.com>
2025-01-24 15:23:12 +0300

    refactor voice-to-text feature

--- src/telegram/voice_to_text_feature/__init__.py
+++ src/telegram/voice_to_text_feature/__init__.py
@@ -27,7 +27,6 @@ class VoiceToText:
         self.speechkit = speechkit
         self.users = users
         self.lamar_tag = settings['lamar-tag']
-        self.file_size_cap = settings.get('file-size-cap', 1024 * 1024)
         self.file_len_cap = settings.get('file-len-cap', 30)
 
         @bot.message_handler(func=self.check_voice, content_types=['text', 'voice'])
@@ -47,35 +46,7 @@ class VoiceToText:
                message.reply_to_message is not None and
                message.reply_to_message.voice is not None)
 
-    
-    async def process_voice(self, message):
-        if self.users.get_user(message.from_user.id) is None:
-            await self.bot.reply_to(message, 'Я тебя еще не знаю. Чтобы я тебя запомнил, '
-                                             'попиши что-то в чате, где есть я')
-            return
-                    
-        if message.voice is None:
-            message = message.reply_to_message
-
-        if message.voice.file_size > self.file_size_cap:
-            await self.bot.reply_to(message,
-                                    f'Файл должен быть не больше {self.file_size_cap} байт')
-            return
-
-        if message.voice.duration > self.file_len_cap:
-            await self.bot.reply_to(message,
-                                    f'Файл должен быть не длиннее {self.file_len_cap} секунд')         
-            return
        
-        voice_url = await self.bot.get_file_url(message.voice.file_id)
-        async with aiohttp.ClientSession() as session:
-            async with session.get(voice_url) as response:
-                voice_file = await response.read()
-
-        text = await self.speechkit.recognize(voice_file)
-        await self.bot.reply_to(message, ('"' + text + '"' ) if len(text.strip()) != 0 else '*Никто ничего не сказал*')
-
-
     def check_video(self, message):
         if message.text != self.lamar_tag and message.chat.type != 'private':
             return False
@@ -84,38 +55,64 @@ class VoiceToText:
                message.reply_to_message is not None and
                message.reply_to_message.video_note is not None)
 
+    
+    async def process_voice(self, message):
+        if not await self._validate_user(message):
+            return
+
+        media_message = await self._get_media_message(message)
+        if media_message is None:
+            return
+      
+        assert media_message.voice is not None
+        voice_bytes = await self._download(media_message.voice)
+
+        text = await self.speechkit.recognize(voice_bytes)
+        await self.bot.reply_to(media_message, f'"{text}"' if len(text.strip()) != 0 else '*Никто ничего не сказал*')
+
        
     async def process_video(self, message):
-        if self.users.get_user(message.from_user.id) is None:
-            await self.bot.reply_to(message, 'Я тебя еще не знаю. Чтобы я тебя запомнил, '
-                                             'попиши что-то в чате, где есть я')
-            return
-                    
-        original_message = message
-        if message.video_note is None:
-            message = message.reply_to_message
-
-        if message.video_note.file_size > self.file_size_cap * 10:
-            await self.bot.reply_to(message,
-                                    f'Файл должен быть не больше {self.file_size_cap} байт')
+        if not await self._validate_user(message):
             return
 
-        if message.video_note.duration > self.file_len_cap:
-            await self.bot.reply_to(message,
-                                    f'Файл должен быть не длиннее {self.file_len_cap} секунд')
+        media_message = await self._get_media_message(message)
+        if media_message is None:
             return
        
-        video_note_url = await self.bot.get_file_url(message.video_note.file_id)
-        async with aiohttp.ClientSession() as session:
-            async with session.get(video_note_url) as response:
-                video_note_bytes = await response.read()
-
-        voice_bytes = utils.extract_audio(video_note_bytes, original_message.from_user.id) 
+        assert media_message.video_note is not None
+        video_bytes = await self._download(media_message.video_note)
+        voice_bytes = utils.extract_audio(video_bytes) 
           
-        if len(voice_bytes) > self.file_size_cap:
-            await self.bot.reply_to(message,
-                                    f'Файл должен быть не больше {self.file_size_cap} байт')
-            return
-            
         text = await self.speechkit.recognize(voice_bytes)
-        await self.bot.reply_to(message, ('"' + text + '"') if len(text.strip()) != 0 else '*Никто ничего не сказал*')
+        await self.bot.reply_to(media_message, f'"{text}"' if len(text.strip()) != 0 else '*Никто ничего не сказал*')
+
+
+    async def _validate_user(self, message):
+        if message.chat.type == 'private' and self.users.get_user(message.from_user.id) is None:
+            await self.bot.reply_to(message, 'Я тебя еще не знаю. Чтобы я тебя запомнил, '
+                                             'попиши что-то в чате, где есть я')
+            return False
+        return True
+   
+        
+    async def _get_media_message(self, message):
+        media = utils.choose_media(message)
+        if media is None:
+            message = message.reply_to_message
+        media = utils.choose_media(message)
+        assert media is not None
+       
+        if media.duration > self.file_len_cap:
+            await self.bot.reply_to(message,
+                                    f'Файл должен быть не более {self.file_len_cap} секунд')
+            return None
+
+        return message
+        
+
+    async def _download(self, media):
+        url = await self.bot.get_file_url(media.file_id)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                return await response.read()
+       
--- src/telegram/voice_to_text_feature/utils.py
+++ src/telegram/voice_to_text_feature/utils.py
@@ -3,7 +3,7 @@ import tempfile
 from moviepy import VideoFileClip
 
 
-def extract_audio(video, postfix):
+def extract_audio(video):
     with tempfile.NamedTemporaryFile(suffix='.mp4') as file:
         file.write(video)
         clip = VideoFileClip(file.name)
@@ -13,3 +13,8 @@ def extract_audio(video, postfix):
         voice_bytes = file.read()
 
     return voice_bytes
+
+
+def choose_media(message):
+    media_list = list(media for media in [message.voice, message.video_note] if media is not None)
+    return None if not media_list else media_list[0]
