6da30571d64e3db5ece89159b47a4df85620ec2b
Sasha Kamalov <ishot83@gmail.com>
2025-02-22 14:12:43 +0300

    add method to add private chats commands

--- src/telegram/bot.py
+++ src/telegram/bot.py
@@ -1,4 +1,5 @@
 from telebot.async_telebot import AsyncTeleBot
+from telebot.types import BotCommand, BotCommandScopeAllPrivateChats
 
 
 class Component:
@@ -8,7 +9,17 @@ class Component:
     def create(components, settings):
         self = Component()
         self.bot = AsyncTeleBot(settings['token'])
+        self.private_commands = list()
         return self
+
+    def add_command_for_private_chats(self, command: str, description: str = 'None'):
+        self.private_commands.append(BotCommand(command, description))
+
+    async def start(self):
+        if len(self.private_commands) > 0:
+            await self.bot.set_my_commands(self.private_commands,
+                                            BotCommandScopeAllPrivateChats())
+        await self.bot.polling()
 
     def get(self):
         return self.bot
